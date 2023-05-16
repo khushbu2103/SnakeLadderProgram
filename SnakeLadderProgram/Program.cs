30b480d44ea61c0e3a9487589ca89e64a04d37c5
khushbu <khushbusoni475@gmail.com>
2023-05-17 01:03:11 +0530

    [khushbu] modify Snake & Ladder Program

--- SnakeLadderProgram/Program.cs
+++ SnakeLadderProgram/Program.cs
@@ -11,7 +11,18 @@ namespace SnakeLadderProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+
+            Console.WriteLine("Welcome to Snake & Ladder Program");
+
+            Console.WriteLine("Press anything to start");
             Console.ReadLine();
+
+            int position = 0; // player's starting position
+            int numberOfRolls = 0; // number of times the dice was rolled
+           
+            }
         }
+
     }
-}
+
+
